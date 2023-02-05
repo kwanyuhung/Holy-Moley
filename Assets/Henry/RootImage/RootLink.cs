6f69232a1e42f6e53ae9455267dcb12a2235dccc
kwan macbook <kwanyuhung@gmail.com>
2023-02-05 01:14:51 -0500

    boss update

---
+++ Assets/Henry/RootImage/RootLink.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootLink : MonoBehaviour
+{
+
+    public bool isHead = false;
+    public RootLink headRoot;
+    public bool isDestory = false;
+    public RootLink preRoot;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetClose()
+    {
+        isDestory = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHead)
+        {
+            if (headRoot.isDestory)
+            {
+                StartCoroutine(DestoryHeadRoot());
+            }
+        }
+        else
+        {
+            if (preRoot.isDestory)
+            {
+                StartCoroutine(DestoryRoot());
+            }
+        }
+    }
+
+    IEnumerator DestoryRoot()
+    {
+        yield return new WaitForSeconds(0.1f);
+        isDestory = true;
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator DestoryHeadRoot()
+    {
+        yield return new WaitForSeconds(0);
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+}
